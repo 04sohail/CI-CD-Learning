836b07cff53b93710b23b79225c5886f684c2f85
ashishkhuranatalentelgia <ashish.khurana@talentelgia.com>
2026-01-06 16:19:00 +0530

    wgwegweg

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-import os
-from dotenv import load_dotenv
+PYTHON_ENV="PROD"
+HOST="0.0.0.0"
+PORT=8000
+PYTHON_WORKER=5
+SERVER_REQUEST_TIMEOUT=300
 
-load_dotenv()
-
-PYTHON_ENV: str = os.getenv("PYTHON_ENV", "DEV")
-
-HOST: str = os.getenv("HOST", "0.0.0.0")
-PORT: int = int(os.getenv("PORT", "9000"))
-PYTHON_WORKER: int = int(os.getenv("PYTHON_WORKER", "1"))
-SERVER_REQUEST_TIMEOUT: int = int(os.getenv("SERVER_REQUEST_TIMEOUT", "300"))
-
+PYTHON_PATH="."
